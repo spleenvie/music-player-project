033a37628d833900707f2d88c057bd4ad0b6b983
spleenvie <vladimirarsene0@gmail.com>
2022-07-13 16:58:40 +0200

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,44 @@
+let allMusic = [
+    {
+        name:"Gazo, Tiakola - Fleurs",
+        artist:"KMT",
+        img:"fleurs",
+        src:"GAZO  - FLEURS",
+    },
+    {
+        name:"Ambre - Tentée",
+        artist:"Reflets",
+        img:"ambre",
+        src:"Ambre - Tentée",
+    },
+    {
+        name:"Ambre, Haili - Grandi",
+        artist:"Reflets",
+        img:"grandi",
+        src:"Ambre  - Grandi",
+    },
+    {
+        name:"Damso - Morose",
+        artist:"QALF INFINITY",
+        img:"damso",
+        src:"Damso - Σ. MOROSE",
+    },
+    {
+        name:"Angèle, Damso - Démons",
+        artist:"95",
+        img:"angele",
+        src:"Mauvais rêves - Angèle (320) (7)",
+    },
+    {
+        name:"Luni, Mister V - Tout Pour Moi",
+        artist:"XX7",
+        img:"luni",
+        src:"Luni - Tout pour Moi",
+    },
+    {
+        name:"Take A Mic, Mister V - Drogue Douce",
+        artist:"INDISPONIBLE",
+        img:"takeamic",
+        src:"Take a Mic, Mister V - Drogue douce",
+    },
+] 
